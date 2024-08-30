bfbad75bfe2cc5a43dff63c95b18367d7c3176ff
Peckmi <peckmi@mail.uc.edu>
2024-08-30 14:32:41 -0400

    after demo

---
+++ Assignment1/Assignment1/Controllers/PracticeController.cs
@@ -0,0 +1,52 @@
+﻿using Assignment1.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics;
+
+namespace Assignment1.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PracticeController : ControllerBase
+    {
+
+        [HttpGet("GetPractice")]
+        public void GetPractice()
+        {
+            Response.StatusCode = StatusCodes.Status201Created;
+            Response.ContentType = "application/json";
+            var response = "{\"Name\":\"Practice\"}";
+            Response.WriteAsync(response);
+            //return "Practice";
+        }
+
+
+        [HttpGet("GetThing")]
+        public IActionResult GetThing()
+        {
+
+            return Ok(new { Name = "Practice" });
+            
+        }
+
+
+        [HttpGet("GetActionResult")]
+        public IActionResult GetActionResult()
+        {
+
+            //return new OkThingResult(new { Name = "Practice" });
+            var response = new ObjectResult(new { Name = "Practice" });
+            response.StatusCode = StatusCodes.Status200OK;
+            return response;
+        }
+
+
+        [HttpPost("PostPractice")]
+        public IActionResult PostPractice([FromBody] Practice practice)
+        {
+
+            return new OkObjectResult(practice.Name);
+        }
+
+    }
+}
---
+++ Assignment1/Assignment1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get([FromQuery]int days)
+        {
+            _logger.LogInformation("Getting weather forecast");
+            return Enumerable.Range(1, days).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost("PostWeatherForecast/{days:int}")]
+        public IEnumerable<WeatherForecast> PostWeatherForecast( int days,[FromBody]object content)
+        {
+            _logger.LogInformation("Getting weather forecast");
+            return Enumerable.Range(1, days).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
